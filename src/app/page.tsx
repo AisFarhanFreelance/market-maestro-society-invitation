e58b9cb009fa2e72a004d531e3a72561190614d6
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-05-25 12:04:08 +0700

    feat: add fade in animation on start at new ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import FadeInWrapper from "@/components/animation/fade-in";
 import EventInvitation from "@/components/event-invitation/event-invitation";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#2E4366] font-kaisei">
-      <EventInvitation />
-    </div>
+    <FadeInWrapper>
+      <div className="min-h-screen bg-[#2E4366] font-kaisei">
+        <EventInvitation />
+      </div>
+    </FadeInWrapper>
   );
 }
