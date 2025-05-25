fc6f3ffc7d578a76bd82a88a4979da477f9b220e
Aldi-H <aldihimawan88@gmail.com>
2025-05-25 09:53:36 +0700

    feat: add font assets for Amoresa, Avenir, Tex Gyre Termes and update globals.css

---
+++ src/lib/fonts/fonts.ts
@@ -0,0 +1,67 @@
+import { Kaisei_Tokumin } from "next/font/google";
+import localFont from "next/font/local";
+
+const amoresa = localFont({
+  src: [
+    {
+      path: "../../../public/assets/fonts/amoresa-font/Andrey-Sharonov-Amoresa-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-amoresa",
+  display: "swap",
+  preload: true,
+  fallback: ["sans-serif"],
+});
+
+const avenir = localFont({
+  src: [
+    {
+      path: "../../../public/assets/fonts/avenir-font/AvenirLTStd-Light.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../public/assets/fonts/avenir-font/AvenirLTStd-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-avenir",
+  display: "swap",
+  preload: true,
+  fallback: ["sans-serif"],
+});
+
+const texGyreTermes = localFont({
+  src: [
+    {
+      path: "../../../public/assets/fonts/tex-gyre-termes/texgyretermes-regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../public/assets/fonts/tex-gyre-termes/texgyretermes-bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-tex-gyre-termes",
+  display: "swap",
+  preload: true,
+  fallback: ["serif"],
+});
+
+const KaiseiTokumin = Kaisei_Tokumin({
+  variable: "--font-kaisei-tokumin",
+  weight: ["400", "500", "700", "800"],
+  subsets: ["latin"],
+  display: "swap",
+  style: "normal",
+  fallback: ["sans-serif"],
+  preload: true,
+  adjustFontFallback: true,
+});
+
+export { amoresa, avenir, texGyreTermes, KaiseiTokumin };
