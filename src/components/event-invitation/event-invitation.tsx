5a80d8b188795d8ee3768ec35d5a77a03e92d88f
Ais Farhan <aisfarhan@192.168.1.127>
2025-05-26 13:37:28 +0700

    fix: intro page

--- src/components/event-invitation/event-invitation.tsx
+++ src/components/event-invitation/event-invitation.tsx
@@ -6,52 +6,48 @@ import SyailendraLogo from "/public/assets/images/syailendra-logo.svg";
 const EventInvitation = () => {
   return (
     <div
-      className="flex min-h-screen flex-col items-center justify-between bg-[#2E4366] bg-contain bg-center bg-no-repeat px-16 py-36 text-white"
+      className="flex min-h-screen flex-col items-center justify-center bg-[#2E4366] bg-contain bg-center bg-no-repeat px-16 py-36 text-white"
       style={{ backgroundImage: 'url("/assets/images/diamond-border.svg")' }}
     >
-      <Image
-        src={SyailendraLogo}
-        alt="SyailendraLogo"
-        className="object-fill"
-      />
-
-      <div className="space-y-8">
-        <div className="items-center text-center">
-          <span className="flex flex-col font-amoresa text-sm font-light capitalize">
-            Dear Valued Guest
-            <span className="font-avenir normal-case">
-              You&apos;re Invited to
-            </span>
-          </span>
-        </div>
-        <div className="items-center space-y-1.5 text-center">
-          <div>
-            <span className="flex flex-col font-amoresa text-4xl font-light capitalize">
-              Syailendra
-              <span className="font-tex-gyre-termes text-xl font-bold">
-                Networking Night
+      <div className="space-y-20">
+        <div className="space-y-12">
+          <div className="">
+            <div className="items-center text-center">
+              <span className="font-roman flex flex-col font-avenir text-sm capitalize">
+                Dear Valued Guest
+                <span className="font-avenir text-sm font-light normal-case ">
+                  You&apos;re Invited to
+                </span>
               </span>
+            </div>
+            <div className="flex w-full flex-col items-center space-y-1.5 text-center">
+              <div className="flex justify-center">
+                <Image
+                  src={SyailendraLogo}
+                  alt="SyailendraLogo"
+                  className="mx-auto"
+                  // width={300}
+                  height={105.9}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center space-y-1 text-center font-avenir">
+            <span>
+              June 14<sup>th</sup>, 2025
             </span>
-          </div>
-          <div>
-            <span>THE MARKET MAESTRO&apos;S SOCIETY</span>
+            <span>Gates open at 5:30pm</span>
           </div>
         </div>
-        <div className="flex flex-col items-center space-y-1 text-center font-avenir">
-          <span>
-            June 14<sup>th</sup>, 2025
-          </span>
-          <span>Gates open at 5:30pm</span>
-        </div>
-      </div>
 
-      <div className="flex justify-center">
-        <div className="rounded-full bg-gradient-to-r from-white/50 to-white/20 p-[2px]">
-          <Link href="/main">
-            <button className="rounded-full bg-[#2d4163] px-8 py-2 font-kaisei text-base text-white uppercase transition hover:bg-[#3b4d70]">
-              Open Invitation
-            </button>
-          </Link>
+        <div className="flex justify-center">
+          <div className="rounded-full bg-gradient-to-r from-white/50 to-white/20 p-[2px]">
+            <Link href="/main">
+              <button className="rounded-full bg-[#2d4163] px-8 py-2 text-base text-white uppercase transition hover:bg-[#3b4d70]">
+                Open Invitation
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
