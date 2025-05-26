1876d9104ebfba00e4375563a47f5b48e204d287
Ais Farhan <aisfarhan@192.168.1.127>
2025-05-26 15:27:19 +0700

    fix: event-description

--- src/components/event-description/event-description.tsx
+++ src/components/event-description/event-description.tsx
@@ -7,36 +7,44 @@ const EventDescription = () => {
       style={{
         backgroundColor: "#2E4366",
         backgroundImage:
-          "linear-gradient(to right, #586984, #586984 7.5px, #2E4366 7.5px, #2E4366)",
-        backgroundSize: "15px 100%",
+          "linear-gradient(to right, #586984, #586984 2px, #2E4366 2px, #2E4366)",
+        backgroundSize: "8.19px 100%",
       }}
     >
       <div className="absolute inset-0 m-8 flex items-center justify-center text-center">
-        <div className="inline-block h-full bg-gradient-to-bl from-[#B89678] via-[#5B3E26]/[0.34] to-[#FFFFFF] p-[1px]">
-          <div className="flex h-full flex-col justify-center space-y-2 bg-white p-8 align-middle text-[#2E4366]">
-            <AnimatedElement>
-              <div className="font-amoresa text-2xl">A Night of</div>
-              <div className="font-tex-gyre-termes text-xl tracking-[0.5em]  uppercase">
-                Strategic
-              </div>
-              <div className="font-tex-gyre-termes text-base ">Networking</div>
-            </AnimatedElement>
-            <div className="text-avenir flex flex-col space-y-8 text-sm">
-              <AnimatedElement>
-                Market Maestros come together for an evening where financial
-                knowledge meets smart conversations. Where every discussion
-                flows like music, and connections shape the future of smart
-                investing.
-              </AnimatedElement>
-              <AnimatedElement>
-                <div className="flex flex-col space-y-1">
-                  <span className="font-bold">Transform Your Networking</span>
-                  <span>
-                    into a stage for breakthrough opportunities, where ambition
-                    meets strategy in perfect harmony.
-                  </span>
+        <div className="h-max[780px] inline-block h-full border-4 border-white bg-transparent p-[4px]">
+          <div className="inline-block h-full border-2 border-white bg-transparent p-[8px]">
+            <div className="inline-block h-full border bg-white p-[1px]">
+              <div className="flex h-full flex-col justify-center space-y-2 bg-white p-8 align-middle text-[#2E4366]">
+                <AnimatedElement>
+                  <div className="font-amoresa text-2xl">A Night of</div>
+                  <div className="font-tex-gyre-termes text-xl tracking-[0.5em]  uppercase">
+                    Strategic
+                  </div>
+                  <div className="font-tex-gyre-termes text-base ">
+                    Networking
+                  </div>
+                </AnimatedElement>
+                <div className="text-avenir flex flex-col space-y-8 text-sm font-light">
+                  <AnimatedElement>
+                    Market Maestros come together for an evening where financial
+                    knowledge meets smart conversations. Where every discussion
+                    flows like music, and connections shape the future of smart
+                    investing.
+                  </AnimatedElement>
+                  <AnimatedElement>
+                    <div className="flex flex-col space-y-1">
+                      <span className="font-medium">
+                        Transform Your Networking
+                      </span>
+                      <span className="flex flex-col font-light">
+                        into a stage for breakthrough opportunities, where
+                        ambition meets strategy in perfect harmony.
+                      </span>
+                    </div>
+                  </AnimatedElement>
                 </div>
-              </AnimatedElement>
+              </div>
             </div>
           </div>
         </div>
