3855a339796dd3016c34f5d2c4bbbb6f1ea10690
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-05-28 06:11:54 +0700

    fix: minor styling fixes

--- src/components/event-invitation/event-invitation.tsx
+++ src/components/event-invitation/event-invitation.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
+import AnimatedElement from "../animation/animated-element";
 import SyailendraLogo from "/public/assets/images/syailendra-logo2.svg";
 
 const EventInvitation = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-contain bg-center">
-      <Image src={SyailendraLogo} alt="Syailendra Logo" />
+      <AnimatedElement>
+        <Image src={SyailendraLogo} alt="Syailendra Logo" />
+      </AnimatedElement>
     </div>
   );
 };
