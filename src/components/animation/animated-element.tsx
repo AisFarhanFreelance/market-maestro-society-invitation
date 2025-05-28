908c418b2f4986a731a85b3e0492d036a668ced1
Ais Farhan <aisfarhan@Aiss-MacBook-Air.local>
2025-05-28 09:28:38 +0700

    fix: positioning

--- src/components/animation/animated-element.tsx
+++ src/components/animation/animated-element.tsx
@@ -2,13 +2,19 @@
 
 import { motion } from "framer-motion";
 
-const AnimatedElement = ({ children }: { children: React.ReactNode }) => {
+const AnimatedElement = ({
+  children,
+  delay = 0,
+}: {
+  children: React.ReactNode;
+  delay?: number;
+}) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: 10 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ amount: 1 }}
-      transition={{ duration: 1, ease: "easeInOut" }}
+      viewport={{ amount: 0.8 }}
+      transition={{ duration: 1, ease: "easeOut", delay }}
     >
       {children}
     </motion.div>
--- src/components/event-invitation/event-invitation.tsx
+++ src/components/event-invitation/event-invitation.tsx
@@ -6,7 +6,7 @@ import SyailendraLogo from "/public/assets/images/syailendra-logo2.svg";
 const EventInvitation = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-contain bg-center">
-      <AnimatedElement>
+      <AnimatedElement delay={1}>
         <Image src={SyailendraLogo} alt="Syailendra Logo" />
       </AnimatedElement>
     </div>
