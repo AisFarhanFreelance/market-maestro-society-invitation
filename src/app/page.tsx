40fa495db680629d62e8f5b876f0a9227dce2e01
Ais Farhan <aisfarhan@Aiss-MacBook-Air.local>
2025-06-13 08:47:21 +0700

    reroll event blocker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FadeInWrapper from "@/components/animation/fade-in";
-// import EventBlocker from "@/components/event-blocker/event-blocker";
-import EventInvitation from "@/components/event-invitation/event-invitation";
+import EventBlocker from "@/components/event-blocker/event-blocker";
+
+// import EventInvitation from "@/components/event-invitation/event-invitation";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
         <div className="relative h-full w-full max-w-md overflow-hidden bg-[#0D254D]">
           <div className="absolute top-[-184px] left-[-217px] h-[368px] w-[368px] rounded-full bg-[#F05625] opacity-85 blur-[250px]" />
           <div className="absolute top-[673px] right-[-184px] h-[368px] w-[368px] rounded-full bg-[#F05625] opacity-85 blur-[250px]" />
-          <EventInvitation />
+          <EventBlocker />
         </div>
       </div>
     </FadeInWrapper>
